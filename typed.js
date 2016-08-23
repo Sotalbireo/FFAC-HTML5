f8481362b2044ac54f446e1183fb24f7ba368527
Sota NAKAGAMI <athene609.cyl@gmail.com>
2016-08-23 23:33:13 +0900

    mypageの見た目ﾁｮｯﾄ更新
    こんなコミット現場じゃｺﾛｺﾛされそう

--- typed.js
+++ typed.js
@@ -1,27 +1,37 @@
 /// <reference path="typings/index.d.ts" />
-class ItemModel extends Backbone.Model {
-    constructor(argument) {
-        super(argument);
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var ItemModel = (function (_super) {
+    __extends(ItemModel, _super);
+    function ItemModel(argument) {
+        _super.call(this, argument);
     }
-    validate(attrs, opts) {
+    ItemModel.prototype.validate = function (attrs, opts) {
         if (!_.isNumber(attrs.atk)) {
             return "Items ATK point is not Number.";
         }
         if (!_.isNumber(attrs.cost)) {
             return "Items Cost is not Number.";
         }
-    }
-}
-class ItemCollections extends Backbone.Collection {
-    constructor(argument) {
-        super(argument);
+    };
+    return ItemModel;
+}(Backbone.Model));
+var ItemCollections = (function (_super) {
+    __extends(ItemCollections, _super);
+    function ItemCollections(argument) {
+        _super.call(this, argument);
         this.model = ItemModel;
         this.url = 'app/data/item.json';
     }
-}
-class ItemView extends Backbone.View {
-    constructor(argument) {
-        super(argument);
+    return ItemCollections;
+}(Backbone.Collection));
+var ItemView = (function (_super) {
+    __extends(ItemView, _super);
+    function ItemView(argument) {
+        _super.call(this, argument);
         this.el = $('#c1 tbody');
         this.template = _.template($('#c2').html());
         this.model = new ItemModel({});
@@ -29,40 +39,45 @@ class ItemView extends Backbone.View {
         this.collection.fetch();
         this.listenTo(this.collection, 'sync', this.perse);
     }
-    perse() {
-        this.collection.each((ele, i) => {
-            if (!this.model.isValid()) {
-                this.collection.remove(ele, { silent: true });
+    ItemView.prototype.perse = function () {
+        var _this = this;
+        this.collection.each(function (ele, i) {
+            if (!_this.model.isValid()) {
+                _this.collection.remove(ele, { silent: true });
             }
         });
         this.render();
-    }
-    render() {
-        this.collection.each((model) => {
-            this.el.append(this.template(model.toJSON()));
+    };
+    ItemView.prototype.render = function () {
+        var _this = this;
+        this.collection.each(function (model) {
+            _this.el.append(_this.template(model.toJSON()));
         });
         return this;
-    }
-}
-class Router extends Backbone.Router {
-    constructor(argument) {
-        super(argument);
+    };
+    return ItemView;
+}(Backbone.View));
+var Router = (function (_super) {
+    __extends(Router, _super);
+    function Router(argument) {
+        _super.call(this, argument);
         this.routes = {
             "": "index",
             "#item": "itemView"
         };
     }
-    initialize() {
+    Router.prototype.initialize = function () {
         _.bindAll(this);
-    }
-    index() {
+    };
+    Router.prototype.index = function () {
         console.log('index');
         new ItemView({});
-    }
-    itemView() {
+    };
+    Router.prototype.itemView = function () {
         console.log('itemView');
-    }
-}
+    };
+    return Router;
+}(Backbone.Router));
 function ready(fn) {
     if (document.readyState != 'loading') {
         fn();
@@ -71,7 +86,7 @@ function ready(fn) {
         document.addEventListener('DOMContentLoaded', fn);
     }
 }
-ready(() => {
+ready(function () {
     new Router({});
     console.log(Backbone.history.start({ pushState: false, root: "" }));
 });
