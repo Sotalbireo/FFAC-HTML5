9911c47c0638fb69b9ee3582c3bce493736443b8
Sota NAKAGAMI <athene609.cyl@gmail.com>
2016-09-02 23:30:54 +0900

    更新（とついでにmapファイルをignoreする準備）

--- app/minesweeper/page.ts
+++ app/minesweeper/page.ts
@@ -1,25 +1,19 @@
-import * as $ from "jquery";
-// import $ = require('jquery');
-// import _ = require('underscore');
+/// <reference path="../../node_modules/jquery/dist/jquery.min.js" />
+/// <reference path="../../node_modules/lodash/lodash.min.js" />
+
 
 class Cell{
 	isOpened: boolean = false
+	isMine: boolean = false
 	neighbourMines: number = 0
-	constructor(arg: any) {
-		// code...
+	constructor(n:number) {
+		this.neighbourMines = n;
+		if(n===9) {
+			this.isMine = true;
+		}
 	}
 	open(){
 		this.isOpened = true;
-
-	}
-}
-
-class MineCell extends Cell {
-	constructor(arg: any){
-		super(arg);
-	}
-	bumb(){
-		// MineSweeper.GameOver();
 	}
 }
 
@@ -38,23 +32,54 @@ class MineCell extends Cell {
 // }
 
 class MineSweeper {
+	private row: number;
+	private col: number;
+	private bombs: number;
+	private map: number[][];
 	constructor(){
-		const row = $('#Table tr').length;
-		const col = $('#Table tr td').length / row;
-		const bombs = Math.floor(row*col*2/9);
-		let mapping = this.arrayInit(row,col);
-		console.dir(mapping);
+		this.gameInit();
+
+		_.each($('button'), (v)=>{
+			let name = _.split(v.name, '_');
+			name[0]--; name[1]--;
+			v.innerHTML = ""+this.map[name[0]][name[1]];
+		});
 
 		$('button').on('click', (ev: any)=>{
 			console.log((ev.target.name));
 		});
 	}
-	positioning(row: number, col: number, bombs: number): void{
-
+	/**
+	 * Set bomb spot
+	 * @param {number} row   [description]
+	 * @param {number} col   [description]
+	 * @param {number} bombs Number of bomb
+	 */
+	setBombs(row: number, col: number, bombs: number): void{
+		let i = 0;
+		do {
+			let r = _.random(0,row-1);
+			let c = _.random(0,col-1);
+			if(this.map[r][c]!==9) {
+				this.map[r][c] = 9;
+				this.setGuideNumber(r,c);
+				i++;
+			}
+		} while (i < bombs);
 	}
-	arrayInit(row: number, col: number): number[]{
+	setGuideNumber(r:number, c:number): void{
+		const area: number[][] = [[-1,-1],[0,-1],[1,-1],[-1,0],[1,0],[-1,1],[0,1],[1,1]];
+		for(let i=0; i<8; i++){
+			const _r = r+area[i][0];
+			const _c = c+area[i][1];
+			if(0 <= _r && _r < this.row && 0 <= _c && _c < this.col) {
+				this.map[_r][_c] += (this.map[_r][_c]===9)? 0:1;
+			}
+		}
+	}
+	arrayInit(row: number, col: number): number[][]{
 		let array = new Array(row);
-		for(let i=0;i<row;i++){
+		for(let i = 0; i < row; i++){
 			let c: number[] = new Array(col);
 			for(let j=0;j<col;j++){
 				c[j] = 0;
@@ -62,6 +87,14 @@ class MineSweeper {
 			array[i] = c;
 		}
 		return array;
+	}
+	gameInit(): void{
+		this.row = $('#Table tr').length;
+		this.col = $('#Table tr td').length / this.row;
+		this.bombs = Math.floor(this.row*this.col*2/9);
+		this.map = this.arrayInit(this.row,this.col);
+		this.setBombs(this.row, this.col, this.bombs);
+
 	}
 }
 
