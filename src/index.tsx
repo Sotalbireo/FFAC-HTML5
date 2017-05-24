28f7358f110abb92673b4bca48d6f75620735a74
Sota SASAKI <athene609.cyl@gmail.com>
2017-05-24 13:48:42 +0900

    ルーティングライブラリNavigoの検討開始

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,9 @@
-class Router extends Backbone.Router {
-	constructor(props:any) {
-		super(props)
-		this.routes = {
-			"":       this.index,
-			"item":   this.itemView,
-			"*splat": "test"
-		}
-	}
-	test = (_i:any) => {
-		console.log('test',_i)
-	}
-	index = () => {
-		console.log('index')
-	}
-	itemView(){
-		console.log('itemView')
-	}
-}
 document.addEventListener('DOMContentLoaded', ()=>{
-	const router = new Router({})
-	router
-	Backbone.history.start({ pushState:false})
+	const router = new Navigo(null, true)
+	router.on({
+		'item': function() {console.log('item')},
+		'': function() {
+			console.log('home')
+		}
+	}).resolve()
 })
